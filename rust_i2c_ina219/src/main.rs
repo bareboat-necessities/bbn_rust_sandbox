a55c044b11d8e15ecbae301d713318910b046171
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-05 17:19:52 -0500

    Update main.rs

--- rust_i2c_ina219/src/main.rs
+++ rust_i2c_ina219/src/main.rs
@@ -2,7 +2,7 @@ use linux_embedded_hal::{I2cdev, Delay};
 use ina219::SyncIna219;
 use ina219::configuration::{Configuration, BusVoltageRange, Gain, Address};
 use ina219::calibration::{Calibration};
-use ina219::measurement::{Measurement, CurrentRegister, PowerRegister};
+use ina219::measurements::{Measurement, CurrentRegister, PowerRegister};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Initialize the I2C device (e.g., /dev/i2c-1 on Raspberry Pi)
