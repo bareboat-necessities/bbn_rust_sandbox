15213075635989e534715b65de0b4cfd4016ef19
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-05 17:32:51 -0500

    Update main.rs

--- rust_i2c_ina219/src/main.rs
+++ rust_i2c_ina219/src/main.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut ina = SyncIna219::new(device, Address::from_byte(0x42)?)?;
 
     // Wait until a result is ready
-    std::thread::sleep(ina.configuration()?.conversion_time().unwrap());
+    std::thread::sleep(ina.configuration().conversion_time().unwrap());
 
     println!("Bus Voltage: {}", ina.bus_voltage().unwrap());
     println!("Shunt Voltage: {}", ina.shunt_voltage().unwrap());
