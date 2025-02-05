c3f724580df5bcaebaa1b956d2dc4f0cef3b5cdc
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-05 17:46:44 -0500

    Update main.rs

--- rust_i2c_ina219/src/main.rs
+++ rust_i2c_ina219/src/main.rs
@@ -7,6 +7,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
     let mut ina = SyncIna219::new(i2c_bus, Address::from_byte(0x42).unwrap());
 
+    ina.set_configuration(Configuration {
+        // Be extra precise, but take some extra time
+        bus_resolution: Resolution::Avg128,
+        shunt_resolution: Resolution::Avg128,
+
+        // We only care about low voltage bus and shunt, values larger are truncated to the max
+        bus_voltage_range: BusVoltageRange::Fsr16v,
+        shunt_voltage_range: ShuntVoltageRange::Fsr40mv,
+
+        // Measure both signals continuously (default)
+        operating_mode: OperatingMode::Continous(MeasuredSignals::ShutAndBusVoltage),
+
+        // Do not perform a reset
+        reset: Reset::Run,
+    })?;
+    
     /*
     // Wait until a result is ready
     std::thread::sleep(ina.configuration().unwrap().conversion_time().unwrap());
