9cb2f469a64a9405680afe223c77b479ebb03fb5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-05 17:34:00 -0500

    Update main.rs

--- rust_i2c_ina219/src/main.rs
+++ rust_i2c_ina219/src/main.rs
@@ -4,8 +4,8 @@ use linux_embedded_hal::I2cdev;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let device = I2cdev::new("/dev/i2c-1");
-    let mut ina = SyncIna219::new(device, Address::from_byte(0x42));
+    let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
+    let mut ina = SyncIna219::new(i2c_bus, Address::from_byte(0x42));
 
     // Wait until a result is ready
     std::thread::sleep(ina.configuration().conversion_time().unwrap());
