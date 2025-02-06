72fed8560a56c037d6cc36e56a2b5630777dd36f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-05 22:15:39 -0500

    Update main.rs

--- rust_i2c_dht12/src/main.rs
+++ rust_i2c_dht12/src/main.rs
@@ -1,64 +1,21 @@
-use embedded_dht_rs::{dht11::Dht11, dht20::Dht20, dht22::Dht22};
-use linux_embedded_hal::{
-    clock::ClockControl, delay::Delay, gpio::{Io, Level, OutputOpenDrain, Pull}, i2c::I2C, peripherals::Peripherals, prelude::*, system::SystemControl
-};
-use fugit::HertzU32;
+use dht_embedded::{Dht22, DhtSensor, NoopInterruptControl};
+use gpio_cdev::{Chip, LineRequestFlags};
+use linux_embedded_hal::{CdevPin, Delay};
+use std::{thread::sleep, time::Duration};
 
-#[entry]
-fn main() -> ! {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-
-    esp_println::logger::init_logger_from_env();
-
-    let delay = Delay::new(&clocks);
-
-    let od_for_dht11 = OutputOpenDrain::new(io.pins.gpio4, Level::High, Pull::None);
-    let od_for_dht22 = OutputOpenDrain::new(io.pins.gpio5, Level::High, Pull::None);
-    let i2c_for_dht20 = I2C::new(
-        peripherals.I2C0, 
-        io.pins.gpio21,
-        io.pins.gpio22, 
-        HertzU32::kHz(400),
-        &clocks
-    );
-
-    let mut dht11 = Dht11::new(od_for_dht11, delay);
-    let mut dht22 = Dht22::new(od_for_dht22, delay);
-    let mut dht20 = Dht20::new(i2c_for_dht20, delay);
+fn main() -> anyhow::Result<()> {
+    let mut gpiochip = Chip::new("/dev/gpiochip0")?;
+    let line = gpiochip.get_line(17)?;
+    let handle = line.request(LineRequestFlags::INPUT | LineRequestFlags::OUTPUT, 1, "dht-sensor")?;
+    let pin = CdevPin::new(handle)?;
+    let mut sensor = Dht22::new(NoopInterruptControl, Delay, pin);
 
     loop {
-        delay.delay(5000.millis());
-
-        match dht11.read() {
-            Ok(sensor_reading) => log::info!(
-                "DHT 11 Sensor - Temperature: {} °C, humidity: {} %",
-                sensor_reading.temperature,
-                sensor_reading.humidity
-            ),
-            Err(error) => log::error!("An error occurred while trying to read sensor: {:?}", error),
+        match sensor.read() {
+            Ok(reading) => println!("{}°C, {}% RH", reading.temperature(), reading.humidity()),
+            Err(e) => eprintln!("Error: {}", e),
         }
 
-        match dht22.read() {
-            Ok(sensor_reading) => log::info!(
-                "DHT 22 Sensor - Temperature: {} °C, humidity: {} %",
-                sensor_reading.temperature,
-                sensor_reading.humidity
-            ),
-            Err(error) => log::error!("An error occurred while trying to read sensor: {:?}", error),
-        }
-
-        match dht20.read() {
-            Ok(sensor_reading) => log::info!(
-                "DHT 20 Sensor - Temperature: {} °C, humidity: {} %",
-                sensor_reading.temperature,
-                sensor_reading.humidity
-            ),
-            Err(error) => log::error!("An error occurred while trying to read sensor: {:?}", error),
-        }
-
-        log::info!("-----");
+        sleep(Duration::from_millis(2100));
     }
 }
