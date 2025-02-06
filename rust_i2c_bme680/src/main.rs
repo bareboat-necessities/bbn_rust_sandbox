47a22f41c17041ab170d8f3f69dad55b1b77891a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-05 21:43:46 -0500

    Update main.rs

--- rust_i2c_bme680/src/main.rs
+++ rust_i2c_bme680/src/main.rs
@@ -1,35 +1,70 @@
+#![no_std]
+
 use bme680::*;
-use linux_embedded_hal::{I2cdev, Delay};
-use std::result;
-use std::time::Duration;
-use std::thread::sleep;
+use core::result;
+use core::time::Duration;
+use embedded_hal::delay::DelayNs;
+use linux_embedded_hal as hal;
+use linux_embedded_hal::Delay;
+use log::info;
 
+fn main() -> result::Result<(), ()> {
+    env_logger::init();
 
-fn main() -> result::Result<(), Error<<hal::I2cdev as i2c::Read>::Error, <hal::I2cdev as i2c::Write>::Error>> {
-    // Initialize I2C device (e.g., /dev/i2c-1 on Raspberry Pi)
-    let i2c = I2cdev::new("/dev/i2c-1").unwrap();
-    let mut delayer = Delay::new(10000);
-    let mut dev = Bme680::init(i2c, &mut delayer, I2CAddress::Primary)?;
+    let i2c = hal::I2cdev::new("/dev/i2c-1").unwrap();
+    let mut delayer = Delay {};
+
+    let mut dev = Bme680::init(i2c, &mut delayer, I2CAddress::Primary).map_err(|e| {
+        log::error!("Error at bme680 init {e:?}");
+    })?;
+
+    let mut delay = Delay {};
+
     let settings = SettingsBuilder::new()
         .with_humidity_oversampling(OversamplingSetting::OS2x)
         .with_pressure_oversampling(OversamplingSetting::OS4x)
         .with_temperature_oversampling(OversamplingSetting::OS8x)
         .with_temperature_filter(IIRFilterSize::Size3)
         .with_gas_measurement(Duration::from_millis(1500), 320, 25)
+        .with_temperature_offset(-2.2)
         .with_run_gas(true)
         .build();
-    dev.set_sensor_settings(&mut delayer, settings)?;
-    let profile_duration = dev.get_profile_dur(&settings.0)?;
 
-    // Read sensor data
-    dev.set_sensor_mode(&mut delayer, PowerMode::ForcedMode)?;
-    sleep(profile_duration);
-    let (data, _state) = dev.get_sensor_data(&mut delayer)?;
+    let profile_dur = dev.get_profile_dur(&settings.0).map_err(|e| {
+        log::error!("Unable to get profile dur {e:?}");
+    })?;
+    info!("Profile duration {:?}", profile_dur);
+    info!("Setting sensor settings");
+    dev.set_sensor_settings(&mut delayer, settings)
+        .map_err(|e| {
+            log::error!("Unable to apply sensor settings {e:?}");
+        })?;
+    info!("Setting forced power modes");
+    dev.set_sensor_mode(&mut delayer, PowerMode::ForcedMode)
+        .map_err(|e| {
+            log::error!("Unable to set sensor mode {e:?}");
+        })?;
 
-    println!("Temperature {}°C", data.temperature_celsius());
-    println!("Pressure {}hPa", data.pressure_hpa());
-    println!("Humidity {}%", data.humidity_percent());
-    println!("Gas Resistence {}Ω", data.gas_resistance_ohm());
+    let sensor_settings = dev.get_sensor_settings(settings.1);
+    info!("Sensor settings: {:?}", sensor_settings);
 
-    Ok(())
+    loop {
+        delay.delay_ms(5000u32);
+        let power_mode = dev.get_sensor_mode();
+        info!("Sensor power mode: {:?}", power_mode);
+        info!("Setting forced power modes");
+        dev.set_sensor_mode(&mut delayer, PowerMode::ForcedMode)
+            .map_err(|e| {
+                log::error!("Unable to set sensor mode {e:?}");
+            })?;
+        info!("Retrieving sensor data");
+        let (data, _state) = dev.get_sensor_data(&mut delayer).map_err(|e| {
+            log::error!("Unable to get sensor data {e:?}");
+        })?;
+        info!("Sensor Data {:?}", data);
+        info!("Temperature {}°C", data.temperature_celsius());
+        info!("Pressure {}hPa", data.pressure_hpa());
+        info!("Humidity {}%", data.humidity_percent());
+        info!("Gas Resistence {}Ω", data.gas_resistance_ohm());
+    }
 }
